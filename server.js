d2c27484495d6a603e0b8bf7d546be47a2e8c68f
GunnySensei <gunnerwagoner@hotmail.com>
2022-01-26 18:24:32 -0600

    return all rows with sql query

--- server.js
+++ server.js
@@ -3,9 +3,32 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require("mysql2");
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Blue86Jeep12!",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
